5f9336fbcc4345ce65a9424987b63fdb4997b1f0
chezapangestu <61898676+chezapangestu@users.noreply.github.com>
2020-04-26 10:36:27 +0700

    Update random-walk.py

--- random-walk.py
+++ random-walk.py
@@ -2,8 +2,11 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Inisialisasi Variabel Dasar
 # variable Scalar
 jumlah_individu = 200  #200 individu
 rasio_terinfeksi = 0.05  #5% : 0.05
 PMove = 0.8  #80% : 0.8
 WPemulihan = 10
+#-------------------------
+jumlah_terinfeksi = int(jumlah_individu * rasio_terinfeksi)
