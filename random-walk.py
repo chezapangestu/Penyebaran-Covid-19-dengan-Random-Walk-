e9d285cd6bfde906123d93bdc97cd39c2275489f
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-26 10:30:51 +0700

    Update random-walk.py

--- random-walk.py
+++ random-walk.py
@@ -1,3 +1,9 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+
+# variable Scalar
+jumlah_individu = 200  #200 individu
+rasio_terinfeksi = 0.05  #5% : 0.05
+PMove = 0.8  #80% : 0.8
+WPemulihan = 10
